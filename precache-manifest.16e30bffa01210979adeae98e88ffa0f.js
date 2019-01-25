4f0477cf8862423163616221145fdc357665b004
Yurii <yurii.oleshko@gmail.com>
2019-01-25 11:35:47 +0200

    Updates

---
+++ precache-manifest.16e30bffa01210979adeae98e88ffa0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f2fbab3b07f758c24352a765bd846da",
+    "url": "/YuriiOleshko/App_Weather/static/media/hdwallpapers.top_190.2f2fbab3.jpg"
+  },
+  {
+    "revision": "d43b4392ebaaab64684f",
+    "url": "/YuriiOleshko/App_Weather/static/js/runtime~main.d43b4392.js"
+  },
+  {
+    "revision": "fa4ca66a47c52999356b",
+    "url": "/YuriiOleshko/App_Weather/static/js/main.fa4ca66a.chunk.js"
+  },
+  {
+    "revision": "fefdf5c3cd9260d922d0",
+    "url": "/YuriiOleshko/App_Weather/static/js/1.fefdf5c3.chunk.js"
+  },
+  {
+    "revision": "fa4ca66a47c52999356b",
+    "url": "/YuriiOleshko/App_Weather/static/css/main.3e999972.chunk.css"
+  },
+  {
+    "revision": "586f7e009d887aa52ecb40db584074b6",
+    "url": "/YuriiOleshko/App_Weather/index.html"
+  }
+];
